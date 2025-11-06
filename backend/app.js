996e839af6c87d6a980777d4ec4c34a64ac1ebd3
santimontironi <santiimontironi@gmail.com>
2025-11-06 18:06:20 -0300

    cambios

--- backend/app.js
+++ backend/app.js
@@ -17,12 +17,13 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 
-app.use(cors( 
-    {
-        origin: process.env.NODE_ENV === "production" ? process.env.FRONTEND_URL : "http://localhost:5173",
-        credentials: true
-    }
-))
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    process.env.FRONTEND_URL
+  ],
+  credentials: true
+}));
 
 app.use(async (req, res, next) => {
   try {
