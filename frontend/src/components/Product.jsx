45ae2d56f8bd089c9d416f0bf45c08b7eb5e5884
santimontironi <santiimontironi@gmail.com>
2025-11-07 19:11:25 -0300

    solucion render imagenes

--- frontend/src/components/Product.jsx
+++ frontend/src/components/Product.jsx
@@ -5,7 +5,7 @@ const Product = ({ id, admin, productName, productPrice, productImage, productDe
     return (
         <div className="w-[280px] mx-auto xl:w-[320px] xl:h-auto flex flex-col rounded-2xl bg-linear-to-br from-[#1a1a1a] to-[#333] hover:shadow-[10px_10px_50px_rgba(0,0,0,0.8)] hover:scale-105 transform transition-all duration-300">
             <div className="flex flex-col gap-3 justify-center text-center text-white p-3">
-                <img className="mb-5 rounded-xl transition-transform duration-500 hover:scale-105" src={`http://localhost:3000/uploads/${productImage}`} alt={productName} />
+                <img className="mb-5 rounded-xl transition-transform duration-500 hover:scale-105" src={productImage} alt={productName} />
                 <h1 className="xl:text-[23px] font-bold text-yellow-400">{productName}</h1>
                 <p className="text-gray-300 text-sm">{productDescription}</p>
                 <span className="text-lg font-semibold text-green-400">${productPrice}</span>
