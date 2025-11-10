116bb9142a09c2f31c16d410abae92b43b773e74
santimontironi <santiimontironi@gmail.com>
2025-11-10 11:59:28 -0300

    solucion importacion

--- backend/controllers/user-controllers.js
+++ backend/controllers/user-controllers.js
@@ -2,7 +2,7 @@ import { Resend } from 'resend'
 
 const resend = new Resend(process.env.RESEND_API_KEY)
 
-export default async function handler(req, res) {
+export const sendMessage = async function (req, res) {
   try {
     const { name, email, message } = req.body
 
