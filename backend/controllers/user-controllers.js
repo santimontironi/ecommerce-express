9d87e6bbf9770db4f2c54f6f791f80a4f34a0ec1
santimontironi <santiimontironi@gmail.com>
2025-11-10 11:53:55 -0300

    volvimos a nodemailer

--- backend/controllers/user-controllers.js
+++ backend/controllers/user-controllers.js
@@ -9,7 +9,7 @@ export const sendMessage = async (req, res) => {
     const transporter = nodemailer.createTransport({
       host: process.env.SMTP_HOST,   
       port: process.env.SMTP_PORT,     
-      secure: process.env.SMTP_PORT, 
+      secure: process.env.SMTP_PORT == 465, 
       auth: {
         user: process.env.SMTP_USER,
         pass: process.env.SMTP_PASS,
